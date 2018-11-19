6791959d3ed23c0015ea4589d8f1021410af97ea
cdc-jtmiller <miller.jeremyt@gmail.com>
2018-11-19 12:20:18 -0500

    Adding files for back-up purposes.

---
+++ R_treeview_ex/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace R_treeview_ex
+{
+    partial class R_Treeview
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.lblDataPathR = new System.Windows.Forms.Label();
+            this.btnChooseFileR = new System.Windows.Forms.Button();
+            this.lblStatTree = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabCode = new System.Windows.Forms.TabPage();
+            this.tbCode = new System.Windows.Forms.TextBox();
+            this.tabLog = new System.Windows.Forms.TabPage();
+            this.tbLog = new System.Windows.Forms.TextBox();
+            this.tabData = new System.Windows.Forms.TabPage();
+            this.lblDataPathNet = new System.Windows.Forms.Label();
+            this.btnChooseFileNet = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabControl1.SuspendLayout();
+            this.tabCode.SuspendLayout();
+            this.tabLog.SuspendLayout();
+            this.tabData.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(9, 96);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(144, 444);
+            this.treeView1.TabIndex = 0;
+            // 
+            // lblDataPathR
+            // 
+            this.lblDataPathR.AutoSize = true;
+            this.lblDataPathR.Location = new System.Drawing.Point(6, 48);
+            this.lblDataPathR.Name = "lblDataPathR";
+            this.lblDataPathR.Size = new System.Drawing.Size(126, 13);
+            this.lblDataPathR.TabIndex = 2;
+            this.lblDataPathR.Text = "Choose data set using R:";
+            // 
+            // btnChooseFileR
+            // 
+            this.btnChooseFileR.Location = new System.Drawing.Point(177, 43);
+            this.btnChooseFileR.Name = "btnChooseFileR";
+            this.btnChooseFileR.Size = new System.Drawing.Size(75, 23);
+            this.btnChooseFileR.TabIndex = 3;
+            this.btnChooseFileR.Text = "Using R";
+            this.btnChooseFileR.UseVisualStyleBackColor = true;
+            this.btnChooseFileR.Click += new System.EventHandler(this.btnChooseFileR_Click);
+            // 
+            // lblStatTree
+            // 
+            this.lblStatTree.AutoSize = true;
+            this.lblStatTree.Location = new System.Drawing.Point(6, 80);
+            this.lblStatTree.Name = "lblStatTree";
+            this.lblStatTree.Size = new System.Drawing.Size(49, 13);
+            this.lblStatTree.TabIndex = 5;
+            this.lblStatTree.Text = "Statistics";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(618, 515);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(96, 25);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(516, 515);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(96, 25);
+            this.btnSubmit.TabIndex = 7;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabCode);
+            this.tabControl1.Controls.Add(this.tabLog);
+            this.tabControl1.Controls.Add(this.tabData);
+            this.tabControl1.Location = new System.Drawing.Point(177, 96);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(537, 413);
+            this.tabControl1.TabIndex = 8;
+            // 
+            // tabCode
+            // 
+            this.tabCode.Controls.Add(this.tbCode);
+            this.tabCode.Location = new System.Drawing.Point(4, 22);
+            this.tabCode.Name = "tabCode";
+            this.tabCode.Padding = new System.Windows.Forms.Padding(3);
+            this.tabCode.Size = new System.Drawing.Size(529, 387);
+            this.tabCode.TabIndex = 0;
+            this.tabCode.Text = "Code";
+            this.tabCode.UseVisualStyleBackColor = true;
+            // 
+            // tbCode
+            // 
+            this.tbCode.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbCode.Location = new System.Drawing.Point(0, 0);
+            this.tbCode.Multiline = true;
+            this.tbCode.Name = "tbCode";
+            this.tbCode.Size = new System.Drawing.Size(529, 387);
+            this.tbCode.TabIndex = 0;
+            // 
+            // tabLog
+            // 
+            this.tabLog.Controls.Add(this.tbLog);
+            this.tabLog.Location = new System.Drawing.Point(4, 22);
+            this.tabLog.Name = "tabLog";
+            this.tabLog.Padding = new System.Windows.Forms.Padding(3);
+            this.tabLog.Size = new System.Drawing.Size(529, 387);
+            this.tabLog.TabIndex = 1;
+            this.tabLog.Text = "Log";
+            this.tabLog.UseVisualStyleBackColor = true;
+            // 
+            // tbLog
+            // 
+            this.tbLog.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbLog.Location = new System.Drawing.Point(0, 0);
+            this.tbLog.Multiline = true;
+            this.tbLog.Name = "tbLog";
+            this.tbLog.Size = new System.Drawing.Size(529, 387);
+            this.tbLog.TabIndex = 0;
+            // 
+            // tabData
+            // 
+            this.tabData.Controls.Add(this.dataGridView1);
+            this.tabData.Location = new System.Drawing.Point(4, 22);
+            this.tabData.Name = "tabData";
+            this.tabData.Size = new System.Drawing.Size(529, 387);
+            this.tabData.TabIndex = 2;
+            this.tabData.Text = "Data";
+            this.tabData.UseVisualStyleBackColor = true;
+            // 
+            // lblDataPathNet
+            // 
+            this.lblDataPathNet.AutoSize = true;
+            this.lblDataPathNet.Location = new System.Drawing.Point(6, 19);
+            this.lblDataPathNet.Name = "lblDataPathNet";
+            this.lblDataPathNet.Size = new System.Drawing.Size(138, 13);
+            this.lblDataPathNet.TabIndex = 9;
+            this.lblDataPathNet.Text = "Choose data set using .Net:";
+            // 
+            // btnChooseFileNet
+            // 
+            this.btnChooseFileNet.Location = new System.Drawing.Point(177, 9);
+            this.btnChooseFileNet.Name = "btnChooseFileNet";
+            this.btnChooseFileNet.Size = new System.Drawing.Size(75, 23);
+            this.btnChooseFileNet.TabIndex = 0;
+            this.btnChooseFileNet.Text = "Using .Net";
+            this.btnChooseFileNet.Click += new System.EventHandler(this.btnChooseFileNet_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(4, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(520, 378);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // R_Treeview
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(730, 546);
+            this.Controls.Add(this.btnChooseFileNet);
+            this.Controls.Add(this.lblDataPathNet);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lblStatTree);
+            this.Controls.Add(this.btnChooseFileR);
+            this.Controls.Add(this.lblDataPathR);
+            this.Controls.Add(this.treeView1);
+            this.Name = "R_Treeview";
+            this.Text = "R_Treeview";
+            this.Load += new System.EventHandler(this.R_Treeview_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabCode.ResumeLayout(false);
+            this.tabCode.PerformLayout();
+            this.tabLog.ResumeLayout(false);
+            this.tabLog.PerformLayout();
+            this.tabData.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label lblDataPathR;
+        private System.Windows.Forms.Button btnChooseFileR;
+        private System.Windows.Forms.Label lblStatTree;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabCode;
+        private System.Windows.Forms.TabPage tabLog;
+        private System.Windows.Forms.TextBox tbLog;
+        private System.Windows.Forms.TextBox tbCode;
+        private System.Windows.Forms.TabPage tabData;
+        private System.Windows.Forms.Label lblDataPathNet;
+        private System.Windows.Forms.Button btnChooseFileNet;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ R_treeview_ex/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RDotNet;
+using RDotNet.NativeLibrary;
+
+namespace R_treeview_ex
+{
+
+    public partial class R_Treeview : Form
+    {
+        public R_Treeview()
+        {
+            InitializeComponent();
+            tabControl1.Selected += new TabControlEventHandler(tabControl1_Selected);
+        }
+
+        private void R_Treeview_Load(object sender, EventArgs e)
+        {
+            // Takes the contents of console and sends it to a textbox
+            Console.SetOut(new ControlWriter(tbLog));
+
+            // Initializes RDotNet
+            MyFunctions.InitializeRDotNet();
+
+            // Populate the treeview
+            ListDirectory(treeView1, @"C:\Work\VS_projects\Projects\R_treeview_ex\User_library");
+        }
+
+        // Used for treeview
+        private void ListDirectory(TreeView treeView, string path)
+        {
+            treeView.Nodes.Clear();
+            var fileDirectory = new DirectoryInfo(path);
+
+            treeView.Nodes.Add(CreateDirectory(fileDirectory));
+        }
+
+        // Used for treeview - creates list files
+        private static TreeNode CreateDirectory(DirectoryInfo directoryInfo)
+        {
+            var directoryNode = new TreeNode(directoryInfo.Name);
+            foreach (var directory in directoryInfo.GetDirectories())
+            {
+                directoryNode.Nodes.Add(CreateDirectory(directory));
+            }
+
+            foreach (var file in directoryInfo.GetFiles())
+            {
+                directoryNode.Nodes.Add(new TreeNode(file.Name));
+            }
+
+            return directoryNode;
+
+        }
+
+        private void btnChooseFileR_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MyFunctions._engine.Evaluate("dataset<-read.csv(file.choose(), sep = ',', stringsAsFactors = FALSE)");
+                DataFrame df = MyFunctions._engine.Evaluate("dataset").AsDataFrame();
+
+                for (int i = 0; i < df.ColumnCount; ++i)
+                {
+                    dataGridView1.ColumnCount++;
+                    dataGridView1.Columns[i].Name = df.ColumnNames[i];
+                }
+
+                for (int i = 0; i < df.RowCount; ++i)
+                {
+                    dataGridView1.RowCount++;
+                    dataGridView1.Rows[i].HeaderCell.Value = df.RowNames[i];
+
+                    for (int k = 0; k < df.ColumnCount; ++k)
+                    {
+                        dataGridView1[k, i].Value = df[i, k];
+                    }
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error loading data using R: " + ex.Message);
+            }
+        }
+
+        private void tabControl1_Selected(object sender, TabControlEventArgs e)
+        {
+            switch ((sender as TabControl).SelectedIndex)
+            {
+                case 0:
+                    btnSubmit.Enabled = true;
+                    break;
+                case 1:
+                    btnSubmit.Enabled = false;
+                    break;
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            if (tabControl1.SelectedIndex == 0)
+            {
+                tbCode.Text = "";
+            }
+            else if (tabControl1.SelectedIndex == 1)
+            {
+                tbLog.Text = "";
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SymbolicExpression expression = MyFunctions._engine.Evaluate(tbCode.Text.Trim());
+                if (expression == null) return;
+
+                CharacterVector vector = expression.AsCharacter();
+
+                foreach (var sSubmitThis in vector)
+                {
+                    
+                    {
+                        Console.WriteLine(sSubmitThis);
+                        tabControl1.SelectedIndex = 1;
+
+                    }
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Problem running your R program: " + ex.Message);
+            }
+
+        }
+
+
+        private void btnChooseFileNet_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string strFileNameNet = "";
+                OpenFileDialog dialogNet = new OpenFileDialog() ;
+                dialogNet.Title = "Open CSV file";
+                dialogNet.Filter = "CSV Files (*.csv)|*.csv";
+                if (dialogNet.ShowDialog() == DialogResult.OK)
+                {
+                    strFileNameNet = dialogNet.FileName;
+                    using (StreamReader sr = new StreamReader(strFileNameNet))
+                    {
+                        DataTable netData = new DataTable();
+                        string[] headers = sr.ReadLine().Split(',');
+                        for (int i = 0; i < headers.Count(); i++)
+                        {
+                            netData.Columns.Add();
+                        }
+                        while (!sr.EndOfStream)
+                        {
+                            string[] rows = sr.ReadLine().Split(',');
+                            DataRow dr = netData.NewRow();
+                            for (int i = 0; i < rows.Count(); i++)
+                            {
+                                dr[i] = rows[i];
+                            }
+                            netData.Rows.Add(dr);
+                            Globals.strDAM = ".Net";
+                        }
+                    }
+                }
+                else
+                {
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error loading data using .Net: " + ex.Message);
+            }
+        }
+    }
+
+
+    public static class MyFunctions
+    {
+        public static REngine _engine;
+
+        public static void InitializeRDotNet()
+        {
+            try
+            {
+                REngine.SetEnvironmentVariables();
+                _engine = REngine.GetInstance();
+                _engine.Initialize();
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error using RDotNet: " + ex.Message);
+            }
+        }
+    }
+
+    public class ControlWriter : TextWriter
+    {
+        private Control textbox;
+        public ControlWriter(Control textbox)
+        {
+            this.textbox = textbox;
+        }
+
+        public override void Write(char value)
+        {
+            textbox.Text += value;
+        }
+
+        public override void Write(string value)
+        {
+            {
+                textbox.Text += value;
+            }
+        }
+
+        public override Encoding Encoding
+        {
+            get { return Encoding.UTF8; }
+        }
+    }
+
+    public static class Globals
+    {
+        public static string strDAM = "UNK";
+    }
+
+}
